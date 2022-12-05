dc661def933206b71c80e2259f224f22f06bde0f
Jordan Clarke <jordan.clarke@thermofisher.com>
2022-12-05 15:17:51 -0800

    Completed day 1 Part two

--- Advent_of_Code/Program.cs
+++ Advent_of_Code/Program.cs
@@ -1,26 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+/* //Day 1: Calorie Counting
+List<int> maxCals = new List<int>{5,4,3};
 List<carrierElf> ListOfElves = calorieCounting.carrierList();
-
-
-//Day 1: Calorie Counting
-int maxCals = 0;
-List<int> topCals = new List<int>(3);
-
 maxCals = GetMaxCals(ListOfElves, maxCals);
-Console.WriteLine($"Day 1 solution: the elf with the most calories is carrying: {maxCals}");
+Console.WriteLine($"Day 1 solution: the elf with the most calories is carrying {maxCals[0]} calories");
+Console.WriteLine($"Together, the top three elves are carrying {maxCals[0] + maxCals[1] + maxCals[2]} calories");
 
 
-
-static int GetMaxCals(List<carrierElf> ListOfElves, int maxCals)
+static List<int> GetMaxCals(List<carrierElf> ListOfElves, List<int> maxCals)
 {
+    int MaxID = 0;
     foreach (carrierElf elf in ListOfElves)
     {
-        if (elf.carriedCalories > maxCals)
+        if (elf.carriedCalories > maxCals[2])
         {
-            maxCals = elf.carriedCalories;
+            maxCals[2] = elf.carriedCalories;
+            if (elf.carriedCalories > maxCals[1])
+            {
+                maxCals[2] = maxCals[1];
+                maxCals[1] = elf.carriedCalories;
+                if (elf.carriedCalories > maxCals[0])
+                {
+                    maxCals[1] = maxCals[0]; 
+                    maxCals[0] = elf.carriedCalories;
+                }
+            }
         }
         //Console.WriteLine(elf.carriedCalories);
     }
     return maxCals;
 }
+
+*/
+

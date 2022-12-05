87b0d2af7cb291a554173eeb3c0889504775ee7c
Jordan Clarke <jordan.clarke@thermofisher.com>
2022-12-05 10:16:35 -0800

    Added Source and Solution Documents for Advent day 1!

--- Advent_of_Code/Program.cs
+++ Advent_of_Code/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+List<carrierElf> ListOfElves = calorieCounting.carrierList();
+
+
+//Day 1: Calorie Counting
+int maxCals = 0;
+foreach (carrierElf elf in ListOfElves)
+{
+    if (elf.carriedCalories > maxCals)
+    {
+        maxCals = elf.carriedCalories;
+    }
+    //Console.WriteLine(elf.carriedCalories);
+}
+Console.WriteLine(maxCals);
+
+
---
+++ Advent_of_Code/calorieCounting.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class calorieCounting
+{
+    public static List<carrierElf> carrierList()
+    {
+        using (var reader = new StreamReader(@"C:\Users\jordan.clarke\source\repos\Advent_of_Code\Advent_of_Code\calorieCounting.txt"))
+        {
+            List<carrierElf> elfList = new List<carrierElf>();
+            int elfID = 0;
+            carrierElf currentElf = new carrierElf(elfID);
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                
+                if (line != "")
+                {
+                    currentElf.carriedItems.Add(Int32.Parse(line));
+                }
+                else if (line == "")
+                {
+                    elfList.Add(currentElf);
+                    elfID++;
+                    currentElf = new carrierElf(elfID);
+                }
+            }
+            return elfList;
+        }
+    }
+}
+public class carrierElf
+{
+    public carrierElf(int ID)
+    {
+        this.carriedItems = new List<int>();
+        this.ID = ID;
+    }
+    public List<int> carriedItems;
+    public int ID;
+    public int carriedCalories => this.carriedItems.Sum();
+
+}
